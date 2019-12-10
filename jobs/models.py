619240c0ed7469b07f718ecd1f2913c76df34a54
abhijit <abhijit.kulkarni@cognitiveclouds.com>
2019-12-10 22:15:37 +0530

    counter

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+class ViewsCount(models.Model):
+    count = models.IntegerField(default=0,blank=True,null=True)
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-from .models import Job
+from .models import Job,ViewsCount
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    views_count = ViewsCount.objects.get(id=1)
+    views_count.count += 1
+    views_count.save()
+    return render(request, 'jobs/home.html', {'jobs':jobs,'count':views_count.count})
