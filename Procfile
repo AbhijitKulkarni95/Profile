a726dbca0640c561a0001c6e23a36785e8795aef
Abhijit Kulkarni <abhijit.kulkarni@cognitiveclouds.com>
2020-12-20 20:15:13 +0530

    changes

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
 web: gunicorn portfolio.wsgi:application
-release: python manage.py crontab add
+release: python corn.py
--- portfolio/corn.py
+++ portfolio/corn.py
@@ -1,3 +1,6 @@
 import requests
+import time
 def my_scheduled_job():
+    time.sleep(2000)
     requests.get('https://abhijitkulkarni.herokuapp.com/')
+    return my_scheduled_job
